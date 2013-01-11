f2ad499b42754410519df396d4fafee92618114b
d1ffuz0r <d1fffuz0r@gmail.com>
2013-01-11 15:51:08 +0400

    Manager for Session, works sequence of calls

--- alchmanager.py
+++ alchmanager.py
@@ -5,26 +5,43 @@ __version__ = '0.0.1'
 __author__ = 'Roman Gladkov'
 
 from sqlalchemy.orm.query import Query
+from sqlalchemy.orm.session import Session
 from sqlalchemy.ext.declarative.api import DeclarativeMeta
 
 __all__ = ['ManagedQuery', 'ManagedSession']
 
-
-not_doubleunder = lambda f: not f.startswith('__')
+not_doubleunder = lambda name: not name.startswith('__')
 
 
 class ManagedQuery(Query):
     """Managed Query object"""
+
     def __init__(self, entities, *args, **kwargs):
         for entity in entities:
             if isinstance(entity, DeclarativeMeta):
                 if hasattr(entity, '__manager__'):
                     manager_cls = entity.__manager__
                     for fname in filter(not_doubleunder, dir(manager_cls)):
-                        method = getattr(manager_cls, fname, None)
-                        setattr(self, fname, lambda: method(self))
+                        fn = getattr(manager_cls, fname)
+                        setattr(self.__class__, fname, fn)
         super(ManagedQuery, self).__init__(entities, *args, **kwargs)
 
 
-class ManagedSession(object):
-    pass
+class ManagedSession(Session):
+
+    methods = {}
+
+    def load_manager(self):
+        def loader(manager_cls):
+            for fname in filter(not_doubleunder, dir(manager_cls)):
+                method = getattr(manager_cls, fname, None)
+                self.methods[fname] = method
+        return loader
+
+    def query(self, *entities, **kwargs):
+        """Return a new ``Query`` object corresponding to this ``Session``."""
+        query = self._query_cls(entities, self, **kwargs)
+        for fname, fn in self.methods.items():
+            if not hasattr(query, fname):
+                setattr(query.__class__, fname, fn)
+        return query
--- tests.py
+++ tests.py
@@ -6,20 +6,36 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 import unittest
-from alchmanager import ManagedQuery
+from alchmanager import ManagedQuery, ManagedSession
 
 
 engine = create_engine('sqlite:///:memory:')
 session = sessionmaker(query_cls=ManagedQuery,
+                       class_=ManagedSession,
                        bind=engine)()
 Base = declarative_base()
 
 
+@session.load_manager()
+class MainSessionManager:
+
+    @staticmethod
+    def published(self):
+        return self.filter_by(is_public=True)
+
+    @staticmethod
+    def has_index(self):
+        return self.filter_by(is_index=True)
+
+
 class MainManager:
-    """Manager with simple custom methods"""
     @staticmethod
     def is_index(self):
         return self.filter_by(is_index=True)
+
+    @staticmethod
+    def is_public(self):
+        return self.filter_by(is_public=True)
 
 
 class Main(Base):
@@ -29,7 +45,7 @@ class Main(Base):
     preview = Column(String(50))
     typeMedia = Column(Integer)
     is_index = Column(Boolean, default=False)
-
+    is_public = Column(Boolean, default=False)
     __manager__ = MainManager
 
     __mapper_args__ = {'polymorphic_on': typeMedia}
@@ -48,15 +64,22 @@ class Test(Base):
     __tablename__ = 'test'
     id = Column(Integer, primary_key=True)
     child = Column(Integer, index=True)
+    is_public = Column(Boolean, default=False)
+    is_index = Column(Boolean)
 
 
 Base.metadata.create_all(engine)
-v = Video(movie='test',
-          preview='test')
-p = Video(movie='test',
-          preview='testmovie',
-          is_index=True)
-session.add_all([v, p])
+items = [Video(movie='test',
+               preview='test'),
+         Video(movie='test',
+               preview='testmovie',
+               is_index=True),
+         Test(child=1,
+              is_public=True,
+              is_index=True),
+         Test(child=2)]
+
+session.add_all(items)
 session.commit()
 
 
@@ -72,7 +95,21 @@ class TestsQueryManager(unittest.TestCase):
         self.assertTrue(session.query(Video).is_index())
 
     def test_table_without_manager(self):
-        self.assertFalse(hasattr(session.query(Test), 'is_index'))
+        self.assertTrue(hasattr(session.query(Test), 'is_index'))
+
+
+class TestsSessionManager(unittest.TestCase):
+
+    def test_is_loaded(self):
+        self.assertTrue(hasattr(session.query(Test), 'published'))
+
+    def test_queries(self):
+        self.assertEquals(session.query(Test).published().count(), 1)
+        self.assertEquals(session.query(Test).count(), 2)
+
+    def test_with_many_calls(self):
+        query = session.query(Test).has_index().published().count()
+        self.assertEquals(query, 1)
 
 
 if __name__ == '__main__':
