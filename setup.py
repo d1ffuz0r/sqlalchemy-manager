9a9c16e2c429241fd0894b9460236533d448d6e6
d1ffuz0r <d1fffuz0r@gmail.com>
2013-04-04 22:49:58 +0400

    prepares for publish to pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
-
+#!/usr/bin/env python
 from setuptools import setup
 
 
-setup(name='alchmanager',
-      version='0.0.1',
-      author='Roman Gladkov',
-      author_email='d1fffuz0r@gmail.com',
-      url='https://github.com/d1ffuz0r/sqlalchemy-manager',
-      install_requires=['sqlalchemy'],
-      py_modules=['alchmanager'],
-      zip_safe=False,
-      test_suite='tests')
+setup(
+    name='alchmanager',
+    version='0.0.1',
+    short_desctiption='Manager for SQLAlchemy',
+    lond_description=open('README.rst').read(),
+    author='Roman Gladkov',
+    author_email='d1fffuz0r@gmail.com',
+    url='https://github.com/d1ffuz0r/sqlalchemy-manager',
+    install_requires=['sqlalchemy'],
+    py_modules=['alchmanager'],
+    zip_safe=False,
+    test_suite='tests',
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+    )
+)
